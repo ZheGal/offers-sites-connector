46f77f945f7c2433170b88be9b7d9742cf207526
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-11-16 10:30:28 +0200

    fix autoload

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function ($class) {
     $prefix = 'App\\';
-    $base_dir = __DIR__;
+    $base_dir = __DIR__ . '/';
 
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
@@ -12,8 +12,7 @@ spl_autoload_register(function ($class) {
     $relative_class = substr($class, $len);
 
     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    echo $file;
-    die;
+    
     if (file_exists ($file)) {
         require ($file);
     }
