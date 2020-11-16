fd554ae4fb654876b5c83473ca2b3b6164217114
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-11-16 10:22:00 +0200

    starts

---
+++ settings_example.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'group' => '0',
+    'offer' => '0',
+    'pid' => '0',
+    'yandex' => '',
+    'facebook' => '',
+    'partner' => [
+        'neogara' => 0,
+        'global' => 1,
+        'partner' => 0
+    ],
+    'language' => '',
+    'sitename' => '',
+    'domain' => '',
+    'cloakit' => ''
+];
