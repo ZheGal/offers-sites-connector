c0f492a17cb2d90ed705392e68413bd697cd440f
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-11-25 12:41:07 +0200

    send mail

--- Classes/Send.php
+++ Classes/Send.php
@@ -9,10 +9,12 @@ use App\Classes\Translate;
 class Send
 {
     public $translate;
+    public $settings;
 
     public function __construct()
     {
         $this->translate = new Translate();
+        $this->get_settings();
         $this->check_empty_fields();
         $this->check_phone_code();
     }
@@ -23,14 +25,52 @@ class Send
         $send = $self->lead_reg();
     }
 
+    public function get_settings()
+    {
+        $settingsPath = implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', 'settings.json']);
+        $settingsRaw = file_get_contents($settingsPath);
+        $settingsJson = json_decode($settingsRaw, 1);
+        $this->settings = $settingsJson;
+        return true;
+    }
+
     public function global()
     {
         $self = new GlobalMaxis();
+        $this->send_mail('zhgalwrk@gmail.com');
     }
 
-    public function send_mail()
+    public function send_mail($mail = '')
     {
-        
+        if (empty($mail)) {
+            return false;
+        }
+
+        $settings = $this->settings;
+        $viewPath = implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Templates', 'mail_send.php']);
+        $_REQUEST['full_url'] = $_SESSION['_ref'];
+        ob_start();
+        require($view);
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        $message = $this->cleanup_message($content);
+        $subject = strval($settings->language.' '.$settings->sitename.' ' . htmlentities($_SERVER["SERVER_NAME"],ENT_COMPAT,'UTF-8'));
+        $form_mail = $this->cleanup_email($_REQUEST['email']);
+        $headers = [
+            'From:  info@'.$_SERVER["SERVER_NAME"],
+            'Reply-To: ' . $form_mail,
+            'X-Mailer: PHP/' . phpversion(),
+            'Content-type: text/html; charset=utf-8'
+        ];
+        $headers = implode("\r\n",$headers);
+        $sent = mail($mail, $subject, $message, $headers);
+        return $sent;
+    }
+
+    public function cleanup_message($message = ''){
+        $message = wordwrap($message, 70, "\r\n");
+        return $message;
     }
 
     public function check_empty_fields()
