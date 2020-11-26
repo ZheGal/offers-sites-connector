477b153782df37db6829445ecb28bc7e44b19b70
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-11-26 14:19:51 +0200

    phone code

--- Classes/Actions.php
+++ Classes/Actions.php
@@ -8,30 +8,7 @@ class Actions
 {
     public static function connectorUpdate()
     {
-        header("Content-type:text/plain");
-        $temp = 'temp_' . substr(md5(rand(0,999999999)), 0, 10) . '_file.sh';
-        $temp = implode(DIRECTORY_SEPARATOR, [__DIR__, $temp]);
-        $filename = 'app_' . substr(md5(rand(0,999999999)), 0, 10) . '_file.zip';
-        $command_list = implode(" && ", [
-            'cd ' . implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..']),
-            'rm -rf app',
-            'mkdir app',
-            'cd app',
-            "wget -O {$filename} https://github.com/ZheGal/offers-sites-connector/archive/main.zip",
-            "unzip -o {$filename}",
-            "rm -rf {$filename}",
-            "cd offers-sites-connector-main",
-            "zip -r app.zip .",
-            "mv app.zip ../",
-            "cd ../",
-            "rm -rf offers-sites-connector-main",
-            "unzip -o app.zip",
-            "rm -rf app.zip 2>&1"
-        ]);
-        
-        $command = exec($command_list . ' 2>&1');
-        echo $command;
-        die;
+        return new \App\Classes\SelfUpdate();
     }
 
     public static function sendForm()
---
+++ Classes/SelfUpdate.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Classes;
+
+class SelfUpdate
+{
+    public $repository = 'https://github.com/ZheGal/offers-sites-connector/archive/main.zip';
+    public $folder = 'offers-sites-connector-main';
+
+    public function __construct()
+    {
+        header("Content-type:text/plain;charset=utf-8");
+        $filename = $this->downloadFunctions(); // скачать функции 
+        $unpack = $this->unpackArchive($filename); // распаковать архив
+        $delete = $this->deleteIsset(); // удалить содержимое папки app
+        $move = $this->moveFromIsset();
+    }
+    
+    public function moveFromIsset()
+    {
+        $app = '../app';
+        $new = "../{$this->folder}";
+        if (rename($new, $app)) {
+            echo 'Success';
+        } else {
+            echo 'Cannot rename new folder to app';
+        }
+    }
+
+    
+    public function downloadFunctions()
+    {
+        $rep = $this->repository;
+        $filename = 'app_' . substr(md5(rand(0,999999999)), 0, 10) . '_file.zip';
+        $load = implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', $filename]);
+        
+        $command = "wget -O {$load} {$rep}";
+        $done = exec($command);
+
+        if (!file_exists($load)) {
+            echo "Cannot download file\n";
+            return false; 
+        }
+        echo 'Archive downloaded'."\n";
+        return [
+            'path' => $load,
+            'filename' => $filename
+        ];
+    }
+
+    public function deleteIsset()
+    {
+        echo 'Deleting app folder';
+        echo exec('rm -rf ../app')."\n";
+    }
+
+    public function unpackArchive($filename)
+    {
+        if (file_exists($filename['path'])) {
+            $root = implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..']);
+
+            $folder = implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', $this->folder]);
+            if (file_exists($folder) && is_dir($folder)) {
+                echo 'Deleting exists folder from archive:';
+                echo exec("rm -rf {$folder}")."\n";
+            }
+            
+            echo "Trying to unzip archive: ";
+            echo exec("cd {$root} && unzip -o {$filename['filename']}") . "\n";
+            
+            echo "Deleting archive: ";
+            echo unlink($filename['path']) . "\n";
+        }
+    }
+}
