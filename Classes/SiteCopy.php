cd6607545ed65ee785910c15936d2a8add3a55c7
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-11-30 15:37:18 +0200

    backuping

--- Classes/SiteCopy.php
+++ Classes/SiteCopy.php
@@ -15,7 +15,7 @@ class SiteCopy
         $this->create_archive();
 
         $result = "https://{$_SERVER['HTTP_HOST']}/backup/public.zip";
-        header("Location: {$result}");
+        echo $result;
     }
 
     public function create_archive()
