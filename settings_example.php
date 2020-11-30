efbcbacdf3a20b21bf8e16446e9ea191c7a75cbd
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-11-30 17:30:15 +0200

    settings example

--- settings_example.php
+++ settings_example.php
@@ -1,14 +1,12 @@
 <?php
 return [
     'group' => '0',
-    'offer' => '0',
     'pid' => 'tyr7dc',
     'return' => 'thanks.php',
     'yandex' => '',
     'facebook' => '',
     'partner' => [
-        'neogara' => 0,
-        'global' => 1,
+        'neogara' => 1,
         'neogara_js' => 0
     ],
     'language' => '',
