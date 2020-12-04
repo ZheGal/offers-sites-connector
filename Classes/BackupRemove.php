b24283c07ba743ad98d0e7acc26960420c525f5f
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-12-04 11:18:55 +0200

    backup remove

--- Classes/BackupRemove.php
+++ Classes/BackupRemove.php
@@ -6,6 +6,32 @@ class BackupRemove
 {
     public function __construct()
     {
-        echo '123';
+        $this->get_filename();
+        $path = $this->backup_path();
+        if ($path) {
+            $this->remove_path();
+        }
+    }
+
+    public function remove_path()
+    {
+        echo 'Deleting exists folder from archive:';
+        echo exec("rm -rf {$this->path}")."\n";
+    }
+
+    public function get_filename()
+    {
+        if (isset($_GET['file'])) {
+            $this->filename = $_GET['file'];
+        }
+    }
+
+    public function backup_path()
+    {
+        $dir = implode(DIRECTORY_SEPARATOR, [__DIR__, '..', '..', 'backup', $this->filename]);
+        if (file_exists($dir)) {
+            $this->path = $dir;
+            return true;
+        }
     }
 }
