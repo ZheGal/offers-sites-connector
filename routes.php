09269db6956c8b9db058e650f0fb4f670eb0efb5
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-12-09 10:31:11 +0200

    link to metrika

--- routes.php
+++ routes.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'metrika/stats.me' => ['GET', 'Actions@LinkToMetrikaStats'],
+    'metrika/stats.php' => ['GET', 'Actions@LinkToMetrikaStats'],
     'api/selfUpdate.me' => ['GET', 'Actions@ConnectorUpdate'],
     'api/copySite.me' => ['GET', 'Actions@MakePublicCopy'],
     'api/backupSite.me' => ['GET', 'Actions@BackupSite'],
