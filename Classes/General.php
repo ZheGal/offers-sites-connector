8261869592ebb9ddcb32c033882779e7e34bdb19
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-12-22 18:48:31 +0200

    location token

--- Classes/General.php
+++ Classes/General.php
@@ -117,7 +117,7 @@ class General
 
     public function change_url_get_ipinfo($view)
     {
-        $url = 'https://' . $_SERVER['HTTP_HOST'] . '/api/getLocation.me';
+        $url = '//' . $_SERVER['HTTP_HOST'] . '/api/getLocation.me';
         $view = str_replace('$.get("https://ipinfo.io", function() {}, "jsonp").', '$.get("'.$url.'", function() {}, "json").', $view);
         $view = str_replace('$.get(\'https://ipinfo.io?\', function() {}, "jsonp")', '$.get(\''.$url.'?\', function() {}, "json")', $view);
         return $view;
