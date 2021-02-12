632dc200e3cf673210b8395a26e1e9714fe859c3
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-02-12 12:42:13 +0200

    update routes and htaccess example

--- routes.php
+++ routes.php
@@ -3,7 +3,9 @@
 return [
     'metrika/stats.php' => ['GET', 'Actions@LinkToMetrikaStats'],
     'api/selfUpdate.me' => ['GET', 'Actions@ConnectorUpdate'],
+    'api/selfUpdate' => ['GET', 'Actions@ConnectorUpdate'],
     'api/updateSettings.me' => ['POST', 'Actions@UpdateSettings'],
     'api/getLocation.me' => ['GET', 'Actions@GetLocation'],
+    'api/getLocation' => ['GET', 'Actions@GetLocation'],
     'send.php' => ['POST', 'Actions@SendForm']
 ];
